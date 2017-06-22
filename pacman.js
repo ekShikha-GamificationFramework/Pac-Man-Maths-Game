6a797c11256f207b3397ed294ff2c2a36e807fb4
Rishabh Gupta <rishabhg1997@gmail.com>
2017-06-22 11:39:02 +0530

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -295,7 +295,9 @@ pacman.update = function(delta) {
     for(var p = 0; p < 4; p++) {
         var distX = Math.abs(ghosts[p].x - this.x);
         var distY = Math.abs(ghosts[p].y - this.y);
-        if(distX + distY < 20 || (ghosts[p].i == this.target_i && ghosts[p].j == this.target_j)) {
+	console.log("ghost = " + ghosts[p].i + ", " +ghosts[p].j);
+	console.log("ghost = " + this.i + ", " +this.j);
+        if(distX + distY < 20 || (ghosts[p].i == this.i && ghosts[p].j == this.j)) {
             //paused = true;
             decreaseLife();
             return;
