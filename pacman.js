103bd8761816e33082648d504df9e07c399263d1
Rishabh Gupta <rishabhg1997@gmail.com>
2017-06-28 11:37:55 +0530

    testing something

--- pacman.js
+++ pacman.js
@@ -1,6 +1,30 @@
 var SCORE;
 var GAMEOVER;
-console.log(window.location.href);
+
+//function to parse the parameters passed in url
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+//reading the x and y parameters
+console.log(JSON.stringify(parseURLParams(window.location.href)));
+
 window.onload = function () {
 
 /*window.onblur = function() {
